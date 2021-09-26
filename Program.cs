88445017e8cab48f62ecc617654ecb711e8d1306
AndersonRochaJC <jmcreggae@gmail.com>
2021-09-25 22:51:59 -0300

    exercicio conluido

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Area_do_triangulo_retangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+           double b, h, area;
+           Console.BackgroundColor = ConsoleColor.Green;
+           Console.WriteLine("Calcule a area...");
+            Console.BackgroundColor = ConsoleColor.Red;
+           Console.Write("Base...: ");
+           b = Convert.ToDouble(Console.ReadLine());
+           Console.BackgroundColor = ConsoleColor.Red;
+           Console.Write("Altura...: ");
+           h = Convert.ToDouble(Console.ReadLine());
+           Console.BackgroundColor = ConsoleColor.Green;
+           area = (b * h) / 2;
+           Console.WriteLine($"Area...: {area:N3} ");
+           Console.ResetColor();
+        }
+        
+    }
+}
